1c3b5279007d14d52ef8ba97ab990ba23c9caea2
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-05-18 22:23:35 +0400

    Add files via upload

--- lab6.2.py
+++ lab6.2.py
@@ -1,17 +1,37 @@
-"""" Сгенерировать все возможные одномерные массивы из чисел 0, 1, 2 и 3, где сумма элементов массива должна быть равна 6.
-Целевая функция заключается в генерации всех уникальных комбинаций, где сумма элементов равна 6. """
-
+"""" Сгенерировать все возможные одномерные массивы из чисел 0, 1, 2 и 3"""
 n = 4
 K = []
-for i in range(4):
-    for j in range(4):
-        for k in range(4):
-            for l in range(4):
-                if i != j and i != k and i != l and j != k and j != l and k != l: # ограничение на повторяющиеся элементы
-                    if i + j + k + l == 6: # ограничение на сумму элементов
-                        K.append([i, j, k, l])
+for i in range(1, 2*n+1):
+    if i % 2 == 0:
+        # четные позиции
+        for j in range(1, n+1, 2):
+            K.append(j)
+        for j in range(2, n+1, 2):
+            K.append(j)
+    else:
+        # нечетные позиции
+        for j in range(2, n+1, 2):
+            K.append(j)
+        for j in range(1, n+1, 2):
+            K.append(j)
 
-print("Количество комбинаций:", len(K))
-print("Все комбинации:")
-for combination in K:
+# Все возможные варианты массива K из чисел 0, 1, 2 и 3
+combinations = []
+for i in range(4**n):
+    combo = []
+    for j in range(n):
+        digit = (i // (4**j)) % 4
+        combo.append(digit)
+    combinations.append(combo)
+
+# Проверка все комбинации суммы и выбрать, которые равны 5 по модулю
+valid_combinations = []
+for combo in combinations:
+    if all((combo[i] % 2) != (i % 2) for i in range(n)):
+        if abs(sum(combo)) == 6:
+            valid_combinations.append(combo)
+
+print("Количество допустимых комбинаций:", len(valid_combinations))
+print("Все допустимые комбинации:")
+for combination in valid_combinations:
     print(combination)
