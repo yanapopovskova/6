5943a122017f3474f66db7c822afbfa78fcef413
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-05-19 15:47:45 +0400

    Add files via upload

--- lab6.2.py
+++ lab6.2.py
@@ -1,37 +1,24 @@
 """" Сгенерировать все возможные одномерные массивы из чисел 0, 1, 2 и 3"""
 n = 4
 K = []
-for i in range(1, 2*n+1):
-    if i % 2 == 0:
-        # четные позиции
-        for j in range(1, n+1, 2):
-            K.append(j)
-        for j in range(2, n+1, 2):
-            K.append(j)
-    else:
-        # нечетные позиции
-        for j in range(2, n+1, 2):
-            K.append(j)
-        for j in range(1, n+1, 2):
-            K.append(j)
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            for l in range(4):
+                K.append([i, j, k, l])
 
-# Все возможные варианты массива K из чисел 0, 1, 2 и 3
-combinations = []
-for i in range(4**n):
-    combo = []
-    for j in range(n):
-        digit = (i // (4**j)) % 4
-        combo.append(digit)
-    combinations.append(combo)
+min_sum = float('inf')
+min_combination = None
 
-# Проверка все комбинации суммы и выбрать, которые равны 6 по модулю
-valid_combinations = []
-for combo in combinations:
-    if all((combo[i] % 2) != (i % 2) for i in range(n)):
-        if abs(sum(combo)) == 6:
-            valid_combinations.append(combo)
+for combination in K:
+    # Проверяем ограничения на четность и нечетность элементов
+    if all(x % 2 == 1 for x in combination[::2]) and all(x % 2 == 0 for x in combination[1::2]):
+        # Вычисляем значение целевой функции
+        sum_mod = sum(abs(x) for x in combination) % 6
+        # Обновляем минимальное значение и комбинацию переменных
+        if sum_mod < min_sum:
+            min_sum = sum_mod
+            min_combination = combination
 
-print("Количество допустимых комбинаций:", len(valid_combinations))
-print("Все допустимые комбинации:")
-for combination in valid_combinations:
-    print(combination)
+print("Минимальное значение целевой функции:", min_sum)
+print("Набор переменных, при котором достигается минимум:", min_combination)
