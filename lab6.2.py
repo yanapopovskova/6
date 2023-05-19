5a492e87c03eaa6a5351927ac7923b7b0135171f
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-05-19 15:58:35 +0400

    Add files via upload

--- lab6.2.py
+++ lab6.2.py
@@ -13,11 +13,12 @@ min_combination = None
 for combination in K:
     # Проверяем ограничения на четность и нечетность элементов
     if all(x % 2 == 1 for x in combination[::2]) and all(x % 2 == 0 for x in combination[1::2]):
-        # Вычисляем значение 
+        # Вычисляем значение целевой функции
         sum_mod = sum(abs(x) for x in combination) % 6
+        # Обновляем минимальное значение и комбинацию переменных
         if sum_mod < min_sum:
             min_sum = sum_mod
             min_combination = combination
 
-print("Минимальное значение целевой функции:", min_sum)
+print("Количество допустимых комбинаций:", sum_mod)
 print("Набор переменных, при котором достигается минимум:", min_combination)
