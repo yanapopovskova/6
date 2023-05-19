f1cc3a6ec0b77c728d837ceaa69d254ba94b5ea7
yanapopovskova <124494674+yanapopovskova@users.noreply.github.com>
2023-05-19 15:50:01 +0400

    Update lab6.2.py

--- lab6.2.py
+++ lab6.2.py
@@ -13,9 +13,8 @@ min_combination = None
 for combination in K:
     # Проверяем ограничения на четность и нечетность элементов
     if all(x % 2 == 1 for x in combination[::2]) and all(x % 2 == 0 for x in combination[1::2]):
-        # Вычисляем значение целевой функции
+        # Вычисляем значение 
         sum_mod = sum(abs(x) for x in combination) % 6
-        # Обновляем минимальное значение и комбинацию переменных
         if sum_mod < min_sum:
             min_sum = sum_mod
             min_combination = combination
